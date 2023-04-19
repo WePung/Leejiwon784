65f68b72f226bf1ec8c07292d5c97e51d7a895c5
WePung <akgre5@naver.com>
2023-04-19 23:08:13 +0900

    update : FindDepartment
    필터 기능 업데이트

--- client/src/components/FindDepartment/FindDepartment.js
+++ client/src/components/FindDepartment/FindDepartment.js
@@ -4,6 +4,7 @@ import{SchoolInfoContext} from '../../App'
 const FindDepartment = () => {
     const schoolInfo = useContext(SchoolInfoContext);
 
+    const fruits = ['apple', 'banana', 'grapes', 'mango', 'orange'];
     const [filterValue, setFilterValue] = useState("");
 
     const onFilterChange = (e) =>{
@@ -14,14 +15,37 @@ const FindDepartment = () => {
         console.log("click btn")
     },[filterValue])
 
+
+
     // includes를 사용해 문자열에 filterValue와 같은 값이 있을 시 true반환
     const test = schoolInfo.map((it)=>{
         const Departments = it.schoolDepartment;
         if(Departments.includes(filterValue)){
-
+            return(
+            <div key={it.schoolUniqueId}>
+                {it.schoolName}
+                {it.schoolDepartment}
+            </div>
+            )
         }
-        
     })
+
+    // const test2 = schoolInfo.map((it)=>{
+    //     const Departments = it.schoolDepartment;
+    //     const departmentsFilter = Departments.filter(item => item.includes(filterValue))
+    //     console.log(departmentsFilter)
+    //     if(departmentsFilter.filter(item => item.includes(filterValue))){
+    //         return true
+    //         // (
+    //         //     <div key={it.schoolUniqueId}>
+    //         //         {it.schoolName}
+    //         //         {it.schoolDepartment}
+    //         //     </div>
+    //         //     )
+    //     }else{
+    //         return false
+    //     }
+    // })
 
     return (
         <div className='findDepartment'>
@@ -37,15 +61,8 @@ const FindDepartment = () => {
                 <button name = "자동차" value="Automotive" onClick={onFilterChange}>자동차</button>
                 </div>
             </div>
-            {schoolInfo.map((item)=>(
-                <div
-                    key = {item.schoolUniqueId}
-                >
-                    {item.schoolName}
-                    <img src={`${item.image}`}/>
-                </div>
-            ))}
             {test}
+            {/* {test2} */}
         </div>
     );
 };
