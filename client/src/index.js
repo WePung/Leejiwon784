07714d3acd53c5224e8eec78d3b5a62d433cf95f
WePung <akgre5@naver.com>
2023-05-02 20:17:12 +0900

    Fix : index.js
    Fix index.js err

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { legacy_createStore as createStore, applyMiddleware, compose } from "red
 import { Provider } from "react-redux";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { createRoot } from 'react-dom/client';
 
 import App from "./App";
 import rootReducer from "./reducer/index";
@@ -16,11 +17,13 @@ const enhancer =
 // 위에서 만든 reducer를 스토어 만들때 넣어줍니다
 const store = createStore(rootReducer, enhancer);
 
-ReactDOM.render(  
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(  
   <React.StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
